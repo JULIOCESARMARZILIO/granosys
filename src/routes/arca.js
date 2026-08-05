e52b17e2d905b0e06c96be6df8bc7abb3d711c08
JULIOCESARMARZILIO <jcmarzilio@gmail.com>
2026-08-05 02:54:44 -0300

    ARCA: exponer consultas y decisiones protegidas

--- src/routes/arca.js
+++ src/routes/arca.js
@@ -1,4 +1,4 @@
-﻿const router = require('express').Router();
+const router = require('express').Router();
 const arcaService = require('../services/arcaService');
 const arcaOfficialClient = require('../services/arcaOfficialClient');
 
